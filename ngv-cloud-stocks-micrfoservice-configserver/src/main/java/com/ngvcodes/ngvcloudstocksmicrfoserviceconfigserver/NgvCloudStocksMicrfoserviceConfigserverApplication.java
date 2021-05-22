9d9fa88aab65c1a8885ba3459d0ebab4364cf214
NIBIN G VARGHESE <mailtonibin2011@gmail.com>
2021-05-22 11:28:22 +0530

    first commit

---
+++ ngv-cloud-stocks-micrfoservice-configserver/src/main/java/com/ngvcodes/ngvcloudstocksmicrfoserviceconfigserver/NgvCloudStocksMicrfoserviceConfigserverApplication.java
@@ -0,0 +1,13 @@
+package com.ngvcodes.ngvcloudstocksmicrfoserviceconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgvCloudStocksMicrfoserviceConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgvCloudStocksMicrfoserviceConfigserverApplication.class, args);
+	}
+
+}
---
+++ ngv-cloud-stocks-micrfoservice-configserver/src/test/java/com/ngvcodes/ngvcloudstocksmicrfoserviceconfigserver/NgvCloudStocksMicrfoserviceConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngvcodes.ngvcloudstocksmicrfoserviceconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgvCloudStocksMicrfoserviceConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
